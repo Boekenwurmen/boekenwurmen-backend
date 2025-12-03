cdfc55e44447edf17ee371e6d2338155bb0f2631
Boaz Crezee <bjcrezee@gmail.com>
2025-12-03 15:09:57 +0100

    book routes en cors toegevoegd en een nu nog nutteloze controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import Express, { Router } from 'express';
 import { getClient, getClients } from '../controllers/clientsController.js';
+import Cors from 'cors';
+import { getNothing } from '../controllers/booksController.ts';
+
 const router: Router = Express.Router();
 
 // router.get('/', (req: Request, res: Response, next: NextFunction) => {
@@ -8,5 +11,9 @@ const router: Router = Express.Router();
 // });
 router.get('/clients', getClients);
 router.get('/clients/:id', getClient);
+
+router.get('/books/:id', getNothing);
+router.get('/books/:id/:id', getNothing);
+router.get('/books/:id/:id/options', getNothing);
 
 export default router;
