cb61819391f246899e0372a70655206edeb47481
Boaz Crezee <bjcrezee@gmail.com>
2025-12-04 12:16:10 +0100

    added correctly-formatted responses with placeholder data for each of the four books routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import Express, { Router } from 'express';
 import { getClient, getClients } from '../controllers/clientsController.js';
 import Cors from 'cors';
-import { getNothing } from '../controllers/booksController.ts';
+import { getBooks, getChoices, getPages, getStory } from '../controllers/booksController.ts';
 
 const router: Router = Express.Router();
 
@@ -12,8 +12,9 @@ const router: Router = Express.Router();
 router.get('/clients', getClients);
 router.get('/clients/:id', getClient);
 
-router.get('/books/:id', getNothing);
-router.get('/books/:id/:id', getNothing);
-router.get('/books/:id/:id/options', getNothing);
+router.get('/books/', getBooks);
+router.get('/books/:bookId', getPages);
+router.get('/books/:bookId/:pageId', getStory);
+router.get('/books/:bookId/:pageId/options', getChoices);
 
 export default router;
